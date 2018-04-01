65ea70a4e365b4b6b9f3e65561ccd440a5534c44
rudeshmukh <rudeshmukh@USHYDRUDESHMUK1.us.deloitte.com>
2018-04-01 19:13:47 +0530

    function Structure modified

--- model/calculateFrequency.js
+++ model/calculateFrequency.js
@@ -1,8 +1,15 @@
 var http=require('http');
+var regex=require('regex');
 var url='';
 var option;
-var text='';
+//var text='';
  exports.setOption=function(url){
+	 if(url===''){
+		 option={'invalid':true};
+		 return option;
+	 }
+	 
+    url=url.replace(/htt.*:\/\//,'');
     var buf=Buffer.from(url);
     var indexofSlash=buf.indexOf('/');
     if(indexofSlash>1){
@@ -10,22 +17,30 @@ var text='';
         var path=buf.slice(indexofSlash,buf.length);
     }
     var option={'host':host.toString(),
-        'path':path.toString()
+        'path':path.toString(),
+        'invalid':false
     };
     return option;
 };
 
-function getTextFromUrl(textData){
-    http.get(option,(res)=>{
+
+exports.getTextFromUrl=function(option,callback){
+	console.log(option)
+	if (option.invalid){
+		console('invalid term');
+		return;
+	}   
+	var text='';
+	http.get(option,(res)=>{
         console.log(`status:${res.statusCode}`);
         res.on('data',(chunk)=>{
             text+=chunk;
         }).on('end',()=>{
-            console.log("Done with geting the data from server");
-            textData(text);
+            var z={'status':res.statusCode,'text':text};
+            callback(null,z);
         });
     }).on('error',(err)=>{
-        console.log('Cant connect to server');
+    	callback(err,null);
     });
 };
-//option=setOption()
+
